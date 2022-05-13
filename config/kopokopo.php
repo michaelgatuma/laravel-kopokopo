8568ebba8ab734c0fbe2e4b53a639ac632f0f7eb
Michael Gatuma <mgates4410@gmail.com>
2022-05-13 16:54:00 +0300

    update config

--- config/kopokopo.php
+++ config/kopokopo.php
@@ -1,36 +1,45 @@
 <?php
 
 return [
-    'curl_ssl_verify' => false,
-    'sandbox' => true,
-    'client_id' => 'BOPgOCAGF0gsUNHG794EfjtmLiMEJ1BfMXjTZ2FrZM8',
-    'client_secret' => '8KGf7aoHzHezjLpHzyF5NdJFd-T-Q1DewYyKrpiBX_s',
-    'api_key' => 'EuavW1N-H1UMk4D-9XPKPudGGZ3yFBiygEwfkWDes_I',
+    // Set this to false if you cannot authenticate due to SSL certificate problem.
+    'curl_ssl_verify' => true,
 
+    // When you are testing in sandbox environment this remains true, to go to a live environment set it to false
+    'sandbox' => true,
+
+    /* The below values should not be hardcoded for security reason. Add these variables in .env
+    *  i.e KOPOKOPO_CLIENT_ID, KOPOKOPO_CLIENT_SECRET, KOPOKOPO_API_KEY
+    */
+    # This is the application id acquired after you create an Authorization application on the kopokopo dashboard
+    'client_id' => env('KOPOKOPO_CLIENT_ID', 'BOPgOCAGF0gsUNHG794EfjtmLiMEJ1BfMXjTZ2FrZM8'),
+
+    # The kopokopo application client secret
+    'client_secret' => env('KOPOKOPO_CLIENT_SECRET', '8KGf7aoHzHezjLpHzyF5NdJFd-T-Q1DewYyKrpiBX_s'),
+
+    # The kopokopo application api key
+    'api_key' => env('KOPOKOPO_API_KEY', 'EuavW1N-H1UMk4D-9XPKPudGGZ3yFBiygEwfkWDes_I'),
+
+    // Define the scope of your applications control on kopokopo transaction. Using company will control transactions for all till numbers regardless
+    'scope' => 'till', //i.e company, till
+
+    // The business till number given to you by Kopokopo
     'till_number' => '8293219',
-    'scope' => 'till',//company or till
+
+    // Preferred transacting currency i.e KES, USD, AUD
+    'currency' => 'KES',
+
+    // Webhooks are a means of getting notified on your laravel application of events in the Kopo Kopo application. i.e https://api-docs.kopokopo.com/#webhooks
+    // Below values will be used to register your webhooks on Kopokopo. For it to work, update the values and use Kopokopo::registerWebhooks() to register
     'webhooks' => [
-        'buygoods_transaction_received' => 'https://upworkstation.com/k2transrec',
-        'buygoods_transaction_reversed' => 'https://upworkstation.com/k2transrev',
-        'b2b_transaction_received' => 'https://upworkstation.com/k2b2btransrec',
-        'm2m_transaction_received' => 'https://upworkstation.com/k2m2mtransrec',
-        'settlement_transfer_completed' => 'https://upworkstation.com/k2settcomp',
-        'customer_created' => 'https://upworkstation.com/k2custcr'
+        'buygoods_transaction_received' => 'https://example.com/k2transrec',
+        'buygoods_transaction_reversed' => 'https://example.com/k2transrev',
+        'b2b_transaction_received' => 'https://example.com/k2b2btransrec',
+        'm2m_transaction_received' => 'https://example.com/k2m2mtransrec',
+        'settlement_transfer_completed' => 'https://example.com/k2settcomp',
+        'customer_created' => 'https://example.com/k2custcr'
     ],
-    'stk_payment_received_webhook'=>'https://upworkstation.com/k2stkreceiv',
-//    'service_name' => 'M-PESA',
-//    'business_number' => '888555',
-//// 'transaction_reference' => '',
-//    'internal_transaction_id' => '',
-//    'transaction_timestamp' => date('c'),
-//    'transaction_type' => 'Paybill',
-//// 'account_number' => '',
-//// 'sender_phone' => '',
-//// 'first_name' => '',
-//// 'middle_name' => '',
-//// 'last_name' => '',
-////'amount' => '',
-//    'currency' => 'KES',
-//    'api_key' => '',
-//    'api_link' => '',
+
+    // This webhook is used to get notified of a successfull mpesa stk payment
+    'stk_payment_received_webhook' => 'https://example.com/k2stkreceiv',
+
 ];
